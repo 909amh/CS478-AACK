502d68a89e03b875613cdf589e6481336f72b2e1
Addison <909amh@gmail.com>
2024-03-17 15:21:07 -0500

    Silhoutting individual image now works

--- src/image_silhoutter.py
+++ src/image_silhoutter.py
@@ -1,5 +1,24 @@
+import cv2
+import numpy as np
+
+class ImageSilhouetter:
+    def __init__(self):
+        pass
+
+    def silhoutte(self, image: np.ndarray) -> np.ndarray:
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return image
+    
+
 def main():
-    print("Hello World!")
+    silhouetter = ImageSilhouetter()
+    image = silhouetter.silhoutte(cv2.imread("images/088.png"))
+    image = cv2.threshold(image, 170, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    image = cv2.bitwise_not(image)
+    cv2.imshow('Image',image)
+    cv2.waitKey(0)
+
 
 
 if (__name__ == "__main__"):
