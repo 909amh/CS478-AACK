a3b8fb472a8c87eb66d96a4be1685645389109b7
Addison <909amh@gmail.com>
2024-03-17 15:34:25 -0500

    Grayscale appears to be working

--- src/image_silhoutter.py
+++ src/image_silhoutter.py
@@ -7,15 +7,26 @@ class ImageSilhouetter:
 
     def silhoutte(self, image: np.ndarray) -> np.ndarray:
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Convert the image to gray scale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Mask image
+        # mask = image[:, 3] != 0
+        # image[mask] = [0, 0, 0, 255]
+        # Grayscale the image in the reverse colorway as intended
+        image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY_INV)[1]
+
+        # Invert the image
+
         return image
     
 
 def main():
+    # Initialize the ImageSilouetter
     silhouetter = ImageSilhouetter()
-    image = silhouetter.silhoutte(cv2.imread("images/088.png"))
-    image = cv2.threshold(image, 170, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    image = cv2.bitwise_not(image)
+
+    # Create a gray scale of the image
+    image = silhouetter.silhoutte(cv2.imread("images/898.png"))
+    
     cv2.imshow('Image',image)
     cv2.waitKey(0)
 
