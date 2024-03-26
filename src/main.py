7d53c88bd27c2cb63985b0c34f7a6b605857b6dc
unknown <ahmadabushammalh@gmail.com>
2024-03-26 13:14:23 -0500

    landing page

--- src/main.py
+++ src/main.py
@@ -14,19 +14,9 @@ app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 def home():
     return render_template('index.html')
 
-<<<<<<< HEAD
 @app.route('/drawings')
 def canvas():
     return render_template('drawings.html')
-=======
-@app.route('/canvas')
-def canvas():
-    return render_template('canvas.html')
-
-@app.route('/results')
-def results():
-    return render_template('results.html')
->>>>>>> 1b06d0b474e72a019ff7aab11aa3be30ccbe9d25
 
 if __name__ == '__main__':
     app.run(debug=True)
